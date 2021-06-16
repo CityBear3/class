47c2b4004796bd57ddd5192f52547e9c7b349593
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-06-16 16:11:38 +0900

    add:ソースコード

---
+++ class_10.cpp
@@ -0,0 +1,55 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+struct Result {
+  long double value;
+  int count;
+};
+
+long double targetFunction(long double x) { return x * x - std::cos(x); }
+
+long double derivativeFanction(long double x) { return 2 * x + std::sin(x); }
+
+long double repeatFunction(long double x) {
+  return x - targetFunction(x) / derivativeFanction(x);
+}
+
+Result newtonMethod(long double x) {
+  int i = 0;
+  long double x_new;
+  while (i < 2147483647) {
+    x_new = repeatFunction(x);
+    // std::cout << std::fixed << std::setprecision(15) << x_new << std::endl;
+    if (std::abs(x_new - x) < 1.0e-10) {
+      break;
+    }
+    x = x_new;
+    i++;
+  }
+
+  Result result;
+  result.value = x_new;
+  result.count = i;
+  return result;
+}
+
+long double input() {
+  std::cout << "Please input init value" << std::endl;
+  std::cout << "> ";
+  long double x;
+  std::cin >> x;
+  return x;
+}
+
+void output(Result result) {
+  std::cout << "試行回数: " << result.count << std::endl;
+  std::cout << "      解: " << std::fixed << std::setprecision(15)
+            << result.value << std::endl;
+}
+
+int main() {
+  long double x = input();
+  Result res = newtonMethod(x);
+  output(res);
+}
